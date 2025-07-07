036aaaf902648ed46067c71286ab6bea3d296af3
Fahad Ahmad <fahadahmedaz3@gmail.com>
2025-07-07 23:49:37 +0500

    Hook has been added

---
+++ Hook/project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+const  restaurantName = useState("Lemon");
+console.log(restaurantName);
+  return (
+    null
+  )
+}
+
+export default App
